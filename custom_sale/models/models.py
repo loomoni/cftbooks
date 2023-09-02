48e0e8ac76dda019dee109d7035d93206959cecc
loomoni <loomonimorwo1@gmail.com>
2023-09-02 22:54:32 +0300

    proforma is added

---
+++ custom_sale/models/models.py
@@ -0,0 +1,44 @@
+import base64
+from io import BytesIO
+
+from odoo import fields, models, api
+
+
+class SaleOrderCustom(models.Model):
+    _inherit = 'sale.order'
+
+    def _default_reference(self):
+        proformaList = self.env['sale.order'].sudo().search_count([])
+        return 10023 + proformaList + 1
+
+    quotation_date = fields.Date(string="Date",  default=fields.Date.today())
+    proforma_sequence = fields.Integer(string="Proforma Sequence", default=_default_reference)
+
+
+    @api.multi
+    def print_quotation(self):
+        print("Quotation printed")
+        return self.env.ref('custom_sale.proforma_invoice_print_pdf_id').report_action(self)
+
+    @api.model
+    def company_info(self):
+        company = self.env.user.company_id
+        logo_data = base64.b64decode(company.logo)
+        return {
+            'name': company.name,
+            'vat': company.vat,
+            'vrn': company.company_registry,
+            'street': company.street,
+            'street2': company.street2,
+            'phone': company.phone,
+            'email': company.email,
+            'website': company.website,
+            'logo': BytesIO(logo_data)
+        }
+
+
+class InheritProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    uom_id = fields.Many2one('uom.uom', string='Unit of Measure', required=True)
+
